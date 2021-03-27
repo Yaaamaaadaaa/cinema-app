bd1e09cac73093800000d8d7310f2045c3909171
Yaaamaaadaaa <shota.dec22@gmil.com>
2021-03-27 16:30:16 +0900

    MovieIndexPageのテストを追加

--- tests/Feature/MovieIndexPageTest.php
+++ tests/Feature/MovieIndexPageTest.php
@@ -5,20 +5,46 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Movie;
 
 class MovieIndexPageTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->seed('MoviesTableSeeder');
+    }
+
     /**
-     * A basic feature test example.
+     * ステータスが200になる.
      *
      * @return void
      */
-    public function testStatusCode()
+    public function test_status_code()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
     }
 
-    
+    /**
+     * 映画の情報が表示されている.
+     *
+     * @test
+     */
+    public function set_movies_data()
+    {
+        $movies = Movie::All();
+        $response = $this->get('/');
+
+        foreach($movies as $movie){
+          $response->assertSee($movie->title);
+          $response->assertSee($movie->detail);
+        }
+    }
+
+
 }
