a30ba7d5e8e2c72a31cfe0828814f596278a3024
Yaaamaaadaaa <shota.dec22@gmil.com>
2021-04-16 19:07:39 +0900

    インデントの修正

--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -1,30 +1,30 @@
 @extends('layout')
 
 @section('content')
-<h4 class="white-text">公開中の作品一覧</h4>
-    @foreach($movies as $movie)
-      <div class="row">
-        <div class="col s12 m6">
-          <div class="card blue-grey darken-4">
-            <div class="card-content white-text">
-              <span class="card-title">{{ $movie->title }}</span>
-              <p>{{ $movie->detail}}</p>
-              <p>上映時間:{{ substr($movie->running_time, 0, 5) }}</p>
-            </div>
-            <a class='dropdown-trigger btn' data-target='dropdown1'>上映時間を見る</a>
-            
-            <ul id='dropdown1' class='dropdown-content black'>
-              @foreach($movie->schedules as $schedule)
-                <li class="card black">
-                  <a class="white-text">
-                    {{ substr($schedule->start_time, 0, 5) }}〜<span class="yellow-text" style="float: right;">購入　></span>
-                  </a>
-                </li>
-              @endforeach
-            </ul>
-
+  <h4 class="white-text">公開中の作品一覧</h4>
+  @foreach($movies as $movie)
+    <div class="row">
+      <div class="col s12 m6">
+        <div class="card blue-grey darken-4">
+          <div class="card-content white-text">
+            <span class="card-title">{{ $movie->title }}</span>
+            <p>{{ $movie->detail}}</p>
+            <p>上映時間:{{ substr($movie->running_time, 0, 5) }}</p>
           </div>
+          <a class='dropdown-trigger btn' data-target='dropdown1'>上映時間を見る</a>
+
+          <ul id='dropdown1' class='dropdown-content black'>
+            @foreach($movie->schedules as $schedule)
+              <li class="card black">
+                <a class="white-text">
+                  SCREEN{{ $schedule->theater_number }} <span style="padding-left: 50px;">{{ substr($schedule->start_time, 0, 5) }}〜</span><span class="yellow-text" style="float: right;">購入　></span>
+                </a>
+              </li>
+            @endforeach
+          </ul>
+
         </div>
       </div>
-    @endforeach
+    </div>
+  @endforeach
 @endsection
